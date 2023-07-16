c32bb170194431a096d827ffc819e219761ba86c
UPantsiukhou <UPantsiukhou@slb.com>
2023-07-16 11:53:43 +0200

    Initial commit

---
+++ apps/traiding-strategies/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'traiding-strategies',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/traiding-strategies',
+};
